09a53fd52086e32a6aef3da023878fa788d3aec3
sakib-xrz <sakibxrz21@gmail.com>
2023-05-31 14:45:52 +0600

    update some apis

--- src/app/modules/menu/menu.controller.ts
+++ src/app/modules/menu/menu.controller.ts
@@ -18,6 +18,25 @@ export const getMenu = async (req: Request, res: Response) => {
   }
 }
 
+export const getAllCategoriesAndImages = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const data = await menuService.getMenuCategoryAndImageFromDatabase()
+    res.status(200).json({
+      success: true,
+      message: 'Successfully load data',
+      data: data,
+    })
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: 'Failed to load data',
+    })
+  }
+}
+
 export const getAllCategories = async (req: Request, res: Response) => {
   try {
     const categories = await menuService.getAllMenuCategoryFromDatabase()
@@ -37,7 +56,13 @@ export const getAllCategories = async (req: Request, res: Response) => {
 export const getMenuByCategories = async (req: Request, res: Response) => {
   try {
     const { category } = req.params
-    const menu = await menuService.getMenuByCategoryFromDatabase(category)
+    const { limit } = req.query
+
+    const parsedLimit = limit ? parseInt(limit as string, 10) : undefined
+    const menu = await menuService.getMenuByCategoryFromDatabase(
+      category,
+      parsedLimit
+    )
     res.status(200).json({
       success: true,
       message: 'Successfully load data',
@@ -53,6 +78,7 @@ export const getMenuByCategories = async (req: Request, res: Response) => {
 
 export default {
   getMenu,
+  getAllCategoriesAndImages,
   getAllCategories,
   getMenuByCategories,
 }
--- src/app/modules/menu/menu.service.ts
+++ src/app/modules/menu/menu.service.ts
@@ -1,8 +1,27 @@
 import { Menu } from './menu.model'
 
 const getAllMenuFromDatabase = async () => {
-  const menu = await Menu.find()
+  const menu = await Menu.find().limit(6)
   return menu
+}
+
+const getMenuCategoryAndImageFromDatabase = async () => {
+  const categories = await Menu.aggregate([
+    {
+      $group: {
+        _id: '$category',
+        image: { $first: '$image' },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        category: '$_id',
+        image: 1,
+      },
+    },
+  ])
+  return categories
 }
 
 const getAllMenuCategoryFromDatabase = async () => {
@@ -10,13 +29,23 @@ const getAllMenuCategoryFromDatabase = async () => {
   return categories
 }
 
-const getMenuByCategoryFromDatabase = async (params: string) => {
-  const menu = await Menu.find({ category: params })
+const getMenuByCategoryFromDatabase = async (
+  category: string,
+  limit?: number
+) => {
+  let menuQuery = Menu.find({ category })
+
+  if (limit) {
+    menuQuery = menuQuery.limit(3)
+  }
+
+  const menu = await menuQuery.exec()
   return menu
 }
 
 export default {
   getAllMenuFromDatabase,
+  getMenuCategoryAndImageFromDatabase,
   getAllMenuCategoryFromDatabase,
   getMenuByCategoryFromDatabase,
 }
